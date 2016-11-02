0aca691faca6085cefd9ab4b8d8032a423b08789
Danil <voitenko.cr@gmail.com>
2016-11-02 20:09:10 +0200

    Initial Commit

---
+++ CrossFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrossFeed
+//
+//  Created by Danil on 11/2/16.
+//  Copyright © 2016 DanilVoitenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
